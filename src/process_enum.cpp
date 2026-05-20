4010c03c45ec6ed1db3be450f4f1d24287311443
imaleim <163207689+imaleim@users.noreply.github.com>
2026-05-20 01:06:41 -0600

    PIA: entrega final — equipo 4

--- src/process_enum.cpp
+++ src/process_enum.cpp
@@ -1,36 +1,58 @@
-include "process_enum.h"
+#include "process_enum.h"
+
 #include <iostream>
 #include <dirent.h>
 #include <cctype>
+#include <fstream>
+#include <string>
 
-void listProcesses() {
-
+void listProcesses()
+{
     DIR *dir;
     struct dirent *ent;
 
     dir = opendir("/proc");
 
-    if (dir != NULL) {
+    if(dir != NULL)
+    {
+        std::cout << "\n[PROCESOS ACTIVOS]\n";
 
-        std::cout << "\n[Procesos activos]\n";
+        while((ent = readdir(dir)) != NULL)
+        {
+            // Detectar directorios numéricos (PIDs)
+            if(isdigit(ent->d_name[0]))
+            {
+                std::string pid = ent->d_name;
 
-        while ((ent = readdir(dir)) != NULL) {
+                // Ruta al archivo que contiene el nombre del proceso
+                std::string path =
+                    "/proc/" + pid + "/comm";
 
-            // Detectar carpetas numéricas (PIDs)
-            if (isdigit(ent->d_name[0])) {
+                std::ifstream processFile(path);
 
-                std::cout << "PID: "
-                          << ent->d_name
-                          << std::endl;
+                std::string processName;
+
+                if(processFile.is_open())
+                {
+                    getline(processFile, processName);
+
+                    std::cout
+                        << "PID: "
+                        << pid
+                        << " | "
+                        << processName
+                        << std::endl;
+
+                    processFile.close();
+                }
             }
-}
+        }
 
         closedir(dir);
-
-    } else {
-
-        std::cout << "No se pudo abrir /proc"
-                  << std::endl;
+    }
+    else
+    {
+        std::cout
+            << "[ERROR] No se pudo abrir /proc\n";
     }
 }
-
