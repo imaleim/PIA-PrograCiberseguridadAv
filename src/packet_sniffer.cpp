189c508208446f440a961bd31cf2fcd87e776ce1
imaleim <163207689+imaleim@users.noreply.github.com>
2026-05-20 01:08:01 -0600

    PIA: entrega final — equipo 4

--- src/packet_sniffer.cpp
+++ src/packet_sniffer.cpp
@@ -1,32 +1,56 @@
 #include <pcap.h>
 #include <iostream>
+
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
+
+#include <arpa/inet.h>
+
 #include "packet_sniffer.h"
 
-void packetHandler(u_char *args,
-                   const struct pcap_pkthdr *header,
-                   const u_char *packet)
+void packetHandler(
+    u_char *args,
+    const struct pcap_pkthdr *header,
+    const u_char *packet)
 {
-    struct ip *ipHeader = (struct ip*)(packet + 14);
+    struct ip *ipHeader =
+        (struct ip*)(packet + 14);
 
+    // Detectar tráfico ICMP
     if(ipHeader->ip_p == IPPROTO_ICMP)
     {
-        std::cout << "[ICMP detectado] Tamaño: "
-                  << header->len
-                  << " bytes\n";
+        std::cout << "\n[ICMP DETECTADO]\n";
+
+        std::cout
+            << "Origen: "
+            << inet_ntoa(ipHeader->ip_src)
+            << std::endl;
+
+        std::cout
+            << "Destino: "
+            << inet_ntoa(ipHeader->ip_dst)
+            << std::endl;
+
+        std::cout
+            << "Tamaño: "
+            << header->len
+            << " bytes\n";
     }
 }
 
 void startSniffer()
 {
     char errbuf[PCAP_ERRBUF_SIZE];
+
     pcap_if_t *interfaces;
 
     if(pcap_findalldevs(&interfaces, errbuf) == -1)
     {
-        std::cerr << "Error encontrando interfaces: "
-                  << errbuf << std::endl;
+        std::cerr
+            << "Error encontrando interfaces: "
+            << errbuf
+            << std::endl;
+
         return;
     }
 
@@ -40,15 +64,22 @@ void startSniffer()
 
     if(handle == nullptr)
     {
-        std::cerr << "No se pudo abrir interfaz\n";
+        std::cerr
+            << "No se pudo abrir interfaz\n";
+
         return;
     }
 
-    std::cout << "[Sniffer activo en interfaz: "
-              << interfaces->name
-              << "]\n";
+    std::cout
+        << "\n[Sniffer activo en interfaz: "
+        << interfaces->name
+        << "]\n";
 
+    // Capturar 10 paquetes
     pcap_loop(handle, 10, packetHandler, nullptr);
 
     pcap_close(handle);
+
+    std::cout
+        << "\n[Sniffer finalizado]\n";
 }
